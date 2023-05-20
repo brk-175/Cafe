1d5b684530b887d63161e7686ee4c6ed1b875a04
Bhushan Kale <bhushankale175@gmail.com>
2023-05-20 20:25:47 +0530

    Created angular app and added neede services

---
+++ src/app/services/snackbar.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SnackbarService {
+  constructor(private snackBar: MatSnackBar) {}
+
+  openSnackBar(message: string, action: string) {
+    if (action == 'error') {
+      this.snackBar.open(message, '', {
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+        duration: 2000,
+        panelClass: ['red-snackbar'],
+      });
+    } else {
+      this.snackBar.open(message, '', {
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+        duration: 2000,
+        panelClass: ['green-snackbar'],
+      });
+    }
+  }
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  url: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  onSignup(data: any) {
+    return this.http.post(this.url + '/user/signup', data);
+  }
+}
