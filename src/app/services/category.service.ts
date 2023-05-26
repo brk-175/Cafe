4c239203fd883d3612de1b5f62e5ddc90a96de58
Bhushan Kale <bhushankale175@gmail.com>
2023-05-26 11:24:45 +0530

    [Added category and change password component]

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  url: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getAllCategories() {
+    return this.http.get(this.url + '/category/all');
+  }
+
+  addCategory(data: any) {
+    return this.http.post(this.url + '/category/add', data);
+  }
+
+  udpateCategory(data: any) {
+    return this.http.put(this.url + '/category/update', data);
+  }
+}
